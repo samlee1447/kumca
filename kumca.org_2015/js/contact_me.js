bdcd696983d97592a64670b7e8dba86260ec07f2
samlee1447 <33071725+samlee1447@users.noreply.github.com>
2018-02-27 16:02:06 -0500

    Add files via upload

---
+++ kumca.org_2015/js/contact_me.js
@@ -0,0 +1,86 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+			var finalMessage = "Name : " + name + "<br>" + "Email : " + email + "<br>" + "phone : " + phone + "<br>" + "Message : " + message;
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                type: "POST",
+                data: {
+				key: 'RavfOK4n1Pdqpy1uQYTUqg',
+				message: {
+				  'from_email': email,
+				  'to': [
+					  {
+						'email': 'lifeinu@hotmail.com',
+						'name': 'Saekwan Jin',
+						'type': 'to'
+					  },
+					  {
+						'email': 'vampire203@hotmail.com',
+						'name': 'Sam Lee',
+						'type': 'to'
+					  }
+					],
+				  'autotext': 'true',
+				  'subject': 'Contact us email from kumca.org',
+				  'html': finalMessage
+				}
+			  },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
